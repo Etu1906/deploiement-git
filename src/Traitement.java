938ec76c238c60aa4da80cc558e77eb0ae65551b
Rebeka-1892 <etu1892@gmail.com>
2023-09-27 11:14:03 +0200

    'fixfeature3'

--- src/Traitement.java
+++ src/Traitement.java
@@ -16,7 +16,7 @@ public class Traitement extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try {
             PrintWriter out = response.getWriter();
-            out.print("Hello Guys");
+            out.print("Hello Girls");
         } catch (Exception e) {
         }
     }
